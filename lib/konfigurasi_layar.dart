564749089762f109ce5e26ad8829015bd049fb18
Anjas Ard <92562531+Jhazz-Code@users.noreply.github.com>
2024-01-05 19:50:23 +0700

    Update konfigurasi_layar.dart
    Update beberapa fungsi

--- lib/konfigurasi_layar.dart
+++ lib/konfigurasi_layar.dart
@@ -15,7 +15,6 @@ class SizeConfig {
   }
 }
 
-// Get the proportionate height as per screen size
 double getProportionateScreenHeight(double inputHeight) {
   double screenHeight = SizeConfig.screenHeight;
   // 812 is the layout height that designer use
